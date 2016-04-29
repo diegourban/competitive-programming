b1c3a45ddea52b9ddf61ffa49ce7627f2fa3e6b4
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-04-29 19:18:50 -0300

    Bonus

---
+++ src/test/java/br/com/urban/codewars/kyu8/BonusTest.java
@@ -0,0 +1,19 @@
+package br.com.urban.codewars.kyu8;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BonusTest {
+	
+	@Test
+	public void deveGanharBonus() {
+		assertEquals("\u00A31000", Bonus.bonusTime(100, true));
+	}
+	
+	@Test
+	public void naoDeveGanharBonus() {
+		assertEquals("\u00A3200", Bonus.bonusTime(200, false));
+	}
+
+}
