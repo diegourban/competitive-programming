1cd505eb1f2646d161c328fffa6dc4a123173b8d
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-04-29 19:12:05 -0300

    MonkeyCounter

---
+++ src/main/java/br/com/urban/codewars/kyu8/MonkeyCounter.java
@@ -0,0 +1,19 @@
+package br.com.urban.codewars.kyu8;
+
+import java.util.stream.IntStream;
+
+public class MonkeyCounter {
+	
+	public static int[] monkeyCount(final int n){
+		int[] result = new int[n];
+		for(int i = 1; i <= n; i++) {
+			result[i-1] = i;
+		}
+		return result;
+	}
+	
+	public static int[] monkeyCountBestPractice(final int n){
+		return IntStream.rangeClosed(1, n).toArray();
+	}
+
+}
