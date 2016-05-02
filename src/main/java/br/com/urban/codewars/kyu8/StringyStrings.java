7bfd24f1fc1a106e0cfe356249d943a9d85ac54d
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-05-02 12:31:31 -0300

    Update StringyStrings.java

--- src/main/java/br/com/urban/codewars/kyu8/StringyStrings.java
+++ src/main/java/br/com/urban/codewars/kyu8/StringyStrings.java
@@ -3,7 +3,13 @@ package br.com.urban.codewars.kyu8;
 public class StringyStrings {
 
 	public static String stringy(int size) {
-		return "TODO";
+		boolean isTrue = true;
+		String result = "";
+		for(int i = 0; i < size; i++) {
+			result += String.valueOf(isTrue ? 1 : 0);
+			isTrue = !isTrue;
+		}
+		return result;
 	}
 
 }
