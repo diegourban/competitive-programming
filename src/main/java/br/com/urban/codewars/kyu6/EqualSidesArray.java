35ee8dc5a4ff42c5d4222513e5baba8a78f71386
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-05-03 19:49:45 -0300

    EqualSidesArray

---
+++ src/main/java/br/com/urban/codewars/kyu6/EqualSidesArray.java
@@ -0,0 +1,9 @@
+package br.com.urban.codewars.kyu6;
+
+public class EqualSidesArray {
+
+	public static int findEvenIndex(int[] arr) {
+		return 0;
+	}
+
+}
---
+++ src/test/java/br/com/urban/codewars/kyu6/EqualSidesArrayTest.java
@@ -0,0 +1,14 @@
+package br.com.urban.codewars.kyu6;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EqualSidesArrayTest {
+
+	@Test
+	public void deveInstanciar() {
+		EqualSidesArray instance = new EqualSidesArray();
+		Assert.assertNotNull(instance);
+	}
+
+}
