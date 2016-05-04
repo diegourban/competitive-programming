09d19d0f47d2f4cc076aa33fffe643ce4bd0ceba
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-05-04 19:22:46 -0300

    find odd

---
+++ src/main/java/br/com/urban/codewars/kyu6/FindOdd.java
@@ -0,0 +1,29 @@
+package br.com.urban.codewars.kyu6;
+
+import java.util.Arrays;
+
+public class FindOdd {
+
+	public static int findIt(int[] numeros) {
+		if(numeros.length == 1) {
+			return numeros[0];
+		}
+		
+		Arrays.sort(numeros);
+		int contagem = 0;
+		for(int i = 0; i < numeros.length; i++) {
+			if(i+1 >= numeros.length) {
+				return numeros[i];
+			}
+			contagem++;
+			if(numeros[i] != numeros[i+1]) {
+				if(contagem % 2 == 1) {
+					return numeros[i];
+				} else {
+					contagem = 0;
+				}
+			}
+		}
+		return 0;
+	}
+}
