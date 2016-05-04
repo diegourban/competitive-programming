ccc96fc6f67e3c72ef267a9aab36d57dd89373a0
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-05-04 18:13:52 -0300

    collatz conjecture

---
+++ src/main/java/br/com/urban/codewars/kyu7/CollatzConjecture.java
@@ -0,0 +1,9 @@
+package br.com.urban.codewars.kyu7;
+
+public class CollatzConjecture {
+
+	public static long conjecture(long x) {
+		return 0;
+	}
+
+}
---
+++ src/test/java/br/com/urban/codewars/kyu7/CollatzConjectureTest.java
@@ -0,0 +1,14 @@
+package br.com.urban.codewars.kyu7;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CollatzConjectureTest {
+	
+	@Test
+	public void deveInstanciar() {
+		CollatzConjecture instance = new CollatzConjecture();
+		Assert.assertNotNull(instance);
+	}
+
+}
