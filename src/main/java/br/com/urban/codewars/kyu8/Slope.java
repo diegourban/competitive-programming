8e807f87998c5d748241ab8cd14fc044f64d3a33
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-05-05 12:06:19 -0300

    Update Slope.java

--- src/main/java/br/com/urban/codewars/kyu8/Slope.java
+++ src/main/java/br/com/urban/codewars/kyu8/Slope.java
@@ -3,7 +3,11 @@ package br.com.urban.codewars.kyu8;
 public class Slope {
 
 	public String slope(int[] points) {
-		return "undefined";
+		try {
+			return String.valueOf((points[3] - points[1]) / (points[2] - points[0]));
+		} catch (ArithmeticException e) {
+			return "undefined";
+		}
 	}
 
 }
