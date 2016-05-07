c6ff0a1f6ebf9ca3f2b5b0a34834933364159a30
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-05-07 20:14:37 -0300

    Miles todo

--- src/main/java/br/com/urban/codewars/kyu8/StringTemplates.java
+++ src/main/java/br/com/urban/codewars/kyu8/StringTemplates.java
@@ -1,13 +1,14 @@
 package br.com.urban.codewars.kyu8;
 
-/*
- * String Templates - Bug Fixing #5
+import java.util.StringJoiner;
 
-Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
- */
 public class StringTemplates {
 
 	public static String buildString(String... args) {
-		return null;
+		StringJoiner joiner = new StringJoiner(", ");
+		for(String s : args) {
+			joiner.add(s);
+		}
+		return "I like " + joiner.toString() + "!";
 	}
 }
--- src/test/java/br/com/urban/codewars/kyu8/StringTemplatesTest.java
+++ src/test/java/br/com/urban/codewars/kyu8/StringTemplatesTest.java
@@ -15,9 +15,9 @@ public class StringTemplatesTest {
 	
 	@Test
     public void fixedTests() {
-      assertEquals(StringTemplates.buildString("Cheese","Milk","Chocolate"),"I like Cheese, Milk, Chocolate!",StringTemplates.buildString("Cheese","Milk","Chocolate"));
-      assertEquals(StringTemplates.buildString("Cheese","Milk"),"I like Cheese, Milk!",StringTemplates.buildString("Cheese","Milk","Chocolate"));
-      assertEquals(StringTemplates.buildString("Chocolate"),"I like Chocolate!",StringTemplates.buildString("Cheese","Milk","Chocolate"));
+      assertEquals("I like Cheese, Milk, Chocolate!",StringTemplates.buildString("Cheese","Milk","Chocolate"));
+      assertEquals("I like Cheese, Milk!",StringTemplates.buildString("Cheese","Milk"));
+      assertEquals("I like Chocolate!",StringTemplates.buildString("Chocolate"));
     }
 
 }
