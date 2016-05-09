08f86a085a159ccbdab8ff5086297c15e0c56e3f
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-05-09 12:17:58 -0300

    Update Counter.java

--- src/main/java/br/com/urban/codewars/kyu8/Counter.java
+++ src/main/java/br/com/urban/codewars/kyu8/Counter.java
@@ -1,18 +1,15 @@
 package br.com.urban.codewars.kyu8;
 
-/*
- * Consider an array of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
-
-For example,
-
-The correct answer would be 17.
-
-Hint: Don't forget to check for bad values like null/undefined
- */
 public class Counter {
-	
+
 	public int countSheeps(Boolean[] arrayOfSheeps) {
-	    return 0;
-	  }
+		int count = 0;
+		for(Boolean b : arrayOfSheeps) {
+			if(b != null && b.booleanValue()) {
+				count++;
+			}
+		}
+		return count;
+	}
 
 }
