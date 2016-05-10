ea4d1858df60ab76d3b98d5f74af80cf549f3f1e
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-05-10 19:29:51 -0300

    Zebulan

--- src/main/java/br/com/urban/codewars/kyu7/ZebulansNightmare.java
+++ src/main/java/br/com/urban/codewars/kyu7/ZebulansNightmare.java
@@ -1,9 +1,17 @@
 package br.com.urban.codewars.kyu7;
 
+import java.util.StringJoiner;
+
 public class ZebulansNightmare {
 
 	public static String zebulansNightmare(final String functionName) {
-		return null;
+		String[] splitted = functionName.split("_");
+		StringJoiner joiner = new StringJoiner("");
+		joiner.add(splitted[0]);
+		for(int i = 1; i < splitted.length; i++) {
+			joiner.add(Character.toUpperCase(splitted[i].charAt(0)) + splitted[i].substring(1, splitted[i].length()));
+		}
+		return joiner.toString();
 	}
 
 }
