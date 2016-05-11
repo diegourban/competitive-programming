41db282f8e00486302a2c0ee73991c0c6e47d4b0
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-05-11 20:10:28 -0300

    para amanhã

---
+++ src/test/java/br/com/urban/codewars/kyu8/BanjoTest.java
@@ -0,0 +1,14 @@
+package br.com.urban.codewars.kyu8;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BanjoTest {
+	
+	@Test
+	public void deveInstanciar() {
+		 Banjo instance = new  Banjo();
+		Assert.assertNotNull(instance);
+	}
+
+}
