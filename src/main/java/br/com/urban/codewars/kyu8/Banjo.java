ecb16f6f9871679101d3cab5911d14191ebfbe70
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-05-12 12:38:05 -0300

    Update Banjo.java

--- src/main/java/br/com/urban/codewars/kyu8/Banjo.java
+++ src/main/java/br/com/urban/codewars/kyu8/Banjo.java
@@ -1,18 +1,12 @@
 package br.com.urban.codewars.kyu8;
 
-/*
- * Create a function which answers the question "Are you playing Banjo?". If your name starts with the letter "R" or lower case "r", you are playing Banjo!
-
-The function takes a name as its only argument, and returns one of the following strings:
-
-C# name + "plays banjo" name + "does not play banjo"
-
-Names given are always valid strings.
- */
 public class Banjo {
-	
+
 	public static String areYouPlayingBanjo(String name) {
-	    return null;
-	  }
+		if(Character.toUpperCase(name.charAt(0)) == 'R') {
+			return name + " plays banjo";
+		}
+		return name + " does not play banjo";
+	}
 
 }
