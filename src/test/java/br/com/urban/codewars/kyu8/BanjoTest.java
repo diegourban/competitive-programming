a03e932cb0ca9bfbba01a11c0e3475f5d9ca758b
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-05-12 12:38:31 -0300

    Update BanjoTest.java

--- src/test/java/br/com/urban/codewars/kyu8/BanjoTest.java
+++ src/test/java/br/com/urban/codewars/kyu8/BanjoTest.java
@@ -1,14 +1,26 @@
 package br.com.urban.codewars.kyu8;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class BanjoTest {
-	
+
 	@Test
 	public void deveInstanciar() {
-		 Banjo instance = new  Banjo();
+		Banjo instance = new Banjo();
 		Assert.assertNotNull(instance);
+	}
+	
+	@Test
+	public void deveTocarSeIniciaComR() {
+		assertEquals("Robson plays banjo", Banjo.areYouPlayingBanjo("Robson"));
+	}
+	
+	@Test
+	public void naoDeveTocarSeIniciaComD() {
+		assertEquals("Dilma does not play banjo", Banjo.areYouPlayingBanjo("Dilma"));
 	}
 
 }
