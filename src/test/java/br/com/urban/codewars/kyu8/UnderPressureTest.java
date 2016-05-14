45bbf04e6b6f29ef1237ba15a62ca57cfe224137
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-05-14 11:03:26 -0300

    Create UnderPressureTest.java

---
+++ src/test/java/br/com/urban/codewars/kyu8/UnderPressureTest.java
@@ -0,0 +1,29 @@
+package br.com.urban.codewars.kyu8;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UnderPressureTest {
+
+	@Test
+	public void deveInstanciar() {
+		UnderPressure instance = new UnderPressure();
+		Assert.assertNotNull(instance);
+	}
+
+	@Test
+	public final void deveDobrar2() {
+		Assert.assertEquals(4, UnderPressure.doubleInteger(2));
+	}
+	
+	@Test
+	public final void deveDobrar0() {
+		Assert.assertEquals(0, UnderPressure.doubleInteger(0));
+	}
+	
+	@Test
+	public final void deveDobrar1() {
+		Assert.assertEquals(2, UnderPressure.doubleInteger(1));
+	}
+
+}
