961527b2d72692fe4e7c678d1c44987870b25924
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-05-14 11:29:06 -0300

    Update smallestUnusedId.js

--- src/main/js/br/com/urban/codewars/kyu8/smallestUnusedId.js
+++ src/main/js/br/com/urban/codewars/kyu8/smallestUnusedId.js
@@ -1,20 +1,20 @@
-function SmallestKata() {
-
+function sortNumber(a,b) {
+    return a - b;
 }
-SmallestKata.prototype.execute = function(ids) {
-	return nextId(ids);
-};
 
 function nextId(ids) {
 	ids.sort(sortNumber);
 	for (var index = 0; index < ids.length; index++) {
-		if(ids[index] != index) {
+		if(ids[index] !== index) {
 			return index;
 		}
 	}
 	return ids.length;
 }
 
-function sortNumber(a,b) {
-    return a - b;
+function SmallestKata() {
+
 }
+SmallestKata.prototype.execute = function(ids) {
+	return nextId(ids);
+};
