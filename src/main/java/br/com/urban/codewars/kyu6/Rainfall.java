b00ea3008e659125d632700b5d84bc35b2f0c879
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-05-14 11:13:10 -0300

    Update Rainfall.java

--- src/main/java/br/com/urban/codewars/kyu6/Rainfall.java
+++ src/main/java/br/com/urban/codewars/kyu6/Rainfall.java
@@ -3,11 +3,12 @@ package br.com.urban.codewars.kyu6;
 public class Rainfall {
 	
 	public static double mean(String town, String strng) {
-		double rainfallRecordSum = 0;
+		double rainfallRecordSum = -1;
 		int size = 1;
         String[] citiesRecord = strng.split("\n");
         for(String cityRecord : citiesRecord) {
-        	if(cityRecord.startsWith(town)) {
+        	if(cityRecord.split(":")[0].equals(town)) {
+        		rainfallRecordSum = 0;
         		String[] monthsRecord = cityRecord.split(":")[1].split(",");
         		size = monthsRecord.length;
         		for(String monthRecord : monthsRecord) {
@@ -21,11 +22,12 @@ public class Rainfall {
 	
     public static double variance(String town, String strng) {
         final double mean = mean(town, strng);
-        double varianceSum = 0;
+        double varianceSum = -1;
         int size = 1;
         String[] citiesRecord = strng.split("\n");
         for(String cityRecord : citiesRecord) {
-        	if(cityRecord.startsWith(town)) {
+        	if(cityRecord.split(":")[0].equals(town)) {
+        		varianceSum = 0;
         		String[] monthsRecord = cityRecord.split(":")[1].split(",");
         		size = monthsRecord.length;
         		for(String monthRecord : monthsRecord) {
