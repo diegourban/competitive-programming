2d367c0000d325a946280387c54d8f0de9465d1d
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-05-16 18:47:53 -0300

    Update CinemaTest.java

--- src/test/java/br/com/urban/codewars/kyu7/CinemaTest.java
+++ src/test/java/br/com/urban/codewars/kyu7/CinemaTest.java
@@ -10,5 +10,18 @@ public class CinemaTest {
 		Cinema instance = new Cinema();
 		Assert.assertNotNull(instance);
 	}
+	
+	/*
+	
+	private static void testing(long actual, long expected) {
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test1() {
+        System.out.println("Fixed Tests: movie");    
+        testing(Movie.movie(500, 15, 0.9), 43);
+        testing(Movie.movie(100, 10, 0.95), 24);
+    }
+    */
 
 }
