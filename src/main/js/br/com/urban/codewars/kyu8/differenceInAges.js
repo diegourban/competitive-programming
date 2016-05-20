afce0fdd817f32c3033092515575d663e958184d
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-05-19 22:03:13 -0300

    melhorias

--- src/main/js/br/com/urban/codewars/kyu8/differenceInAges.js
+++ src/main/js/br/com/urban/codewars/kyu8/differenceInAges.js
@@ -1,5 +1,9 @@
+function sortNumber(a,b) {
+    return a - b;
+}
+
 function differenceInAges(ages){
-	ages.sort(function(a, b){return a-b});
+	ages.sort(sortNumber);
 	var min = ages[0];
 	var max = ages[ages.length-1];
 	var diff = max - min;
