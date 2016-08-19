38ac980d271d09e5bb418b3ff361ed2164267b89
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-08-18 21:30:07 -0300

    swapper

---
+++ src/test/java/br/com/urban/codewars/kyu8/SwapperTest.java
@@ -0,0 +1,26 @@
+package br.com.urban.codewars.kyu8;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SwapperTest {
+	
+	@Test
+	public void deveInstanciar() {
+		Swapper instance = new Swapper(null);
+		Assert.assertNotNull(instance);
+	}
+	
+	@Test
+	public void deveTrocar() {
+		Integer[] args = new Integer[]{1,2};
+        
+        Swapper r= new Swapper(args);
+        r.swapValues();
+        assertEquals("Failed swapping numbers",2,r.arguments[0] );
+        assertEquals("Failed swapping numbers",1,r.arguments[1] );
+	}
+
+}
