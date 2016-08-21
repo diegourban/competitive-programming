24dceb4ca19109d48064610958b900f7b6ca6e99
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-08-20 21:29:10 -0300

    Sequence sum

---
+++ src/main/java/br/com/urban/codewars/kyu5/LargestDifference.java
@@ -0,0 +1,31 @@
+package br.com.urban.codewars.kyu5;
+
+public class LargestDifference {
+
+	/*
+	 * Summary: Write a function which takes an array data of numbers and
+	 * returns the largest difference in indexes j - i such that data[i] <=
+	 * data[j]
+	 * 
+	 * Long Description:
+	 * 
+	 * The largestDifference takes an array of numbers. That array is not
+	 * sorted. Do not sort it or change the order of the elements in any way, or
+	 * their values.
+	 * 
+	 * Consider all of the pairs of numbers in the array where the first one is
+	 * less than or equal to the second one.
+	 * 
+	 * From these, find a pair where their positions in the array are farthest
+	 * apart.
+	 * 
+	 * Return the difference between the indexes of the two array elements in
+	 * this pair.
+	 */
+
+	public static int largestDifference(int[] data) {
+		// code here
+		return 0;
+	}
+
+}
---
+++ src/test/java/br/com/urban/codewars/kyu5/LargestDifferenceTeste.java
@@ -0,0 +1,15 @@
+package br.com.urban.codewars.kyu5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LargestDifferenceTeste {
+	
+	@Test
+	public void deveInstanciar() {
+		LargestDifference instance = new LargestDifference();
+		assertNotNull(instance);
+	}
+
+}
