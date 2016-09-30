460fd7af5c63cec9daf4ab3223f7faf8416fe72c
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-09-30 13:47:38 -0300

    Update RowSumOddNumbersTest.java

--- src/test/java/br/com/urban/codewars/kyu7/RowSumOddNumbersTest.java
+++ src/test/java/br/com/urban/codewars/kyu7/RowSumOddNumbersTest.java
@@ -18,8 +18,28 @@ public class RowSumOddNumbersTest {
 		assertEquals(1, RowSumOddNumbers.rowSumOddNumbers(1));
 	}
 	
-//	@Test
-//	public void deveCalcularIndice2() {
-//		assertEquals(8, RowSumOddNumbers.rowSumOddNumbers(2));
-//	}
+	@Test
+	public void deveCalcularIndice2() {
+		assertEquals(8, RowSumOddNumbers.rowSumOddNumbers(2));
+	}
+	
+	@Test
+	public void deveCalcularIndice3() {
+		assertEquals(27, RowSumOddNumbers.rowSumOddNumbers(3));
+	}
+	
+	@Test
+	public void deveCalcularIndice4() {
+		assertEquals(64, RowSumOddNumbers.rowSumOddNumbers(4));
+	}
+	
+	@Test
+	public void deveCalcularIndice5() {
+		assertEquals(125, RowSumOddNumbers.rowSumOddNumbers(5));
+	}
+	
+	@Test
+	public void deveCalcularIndice42() {
+		assertEquals(74088, RowSumOddNumbers.rowSumOddNumbers(42));
+	}
 }
