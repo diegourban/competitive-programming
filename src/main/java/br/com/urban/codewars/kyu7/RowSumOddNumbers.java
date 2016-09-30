3807742f5165c418588f411b2cb39a4a88bea579
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-09-30 13:47:04 -0300

    Update RowSumOddNumbers.java

--- src/main/java/br/com/urban/codewars/kyu7/RowSumOddNumbers.java
+++ src/main/java/br/com/urban/codewars/kyu7/RowSumOddNumbers.java
@@ -1,8 +1,24 @@
 package br.com.urban.codewars.kyu7;
 
+/**
+https://www.codewars.com/kata/sum-of-odd-numbers
+*/
 public class RowSumOddNumbers {
 
 	public static int rowSumOddNumbers(int n) {
-		return 1;
+		final int offset = ((n * (n + 1)) / 2) - n;
+		final int firstNumber = (offset * 2) + 1;
+		
+		int sum = 0;
+		int nextNumber = firstNumber;
+		for(int i = 0; i < n; i++) {
+			sum += nextNumber;
+			nextNumber += 2;
+		}
+		
+		return sum;
+		
+		//return n * n * n;
 	}
 }
+
