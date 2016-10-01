e1289be85ad329981c2e830bd18d6645c67e2cfc
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-10-01 16:42:37 -0300

    ajustes

---
+++ src/main/java/br/com/urban/codewars/kyu5/CommonDenominators.java
@@ -0,0 +1,13 @@
+package br.com.urban.codewars.kyu5;
+
+/**
+ * https://www.codewars.com/kata/common-denominators
+ *
+ */
+public class CommonDenominators {
+
+	public static String convertFrac(long[][] lst) {
+		return null;
+	}
+
+}
--- src/test/java/br/com/urban/codewars/kyu7/NodeTest.java
+++ src/test/java/br/com/urban/codewars/kyu7/NodeTest.java
@@ -11,41 +11,28 @@ public class NodeTest {
 		assertNotNull(new Node());
 	}
 
-	@Test
-	public void testNull() {
-		try {
-			Node.getNth(null, 0);
-			assertTrue(false);
-		} catch (Exception e) {
-			assertTrue(true);
-		}
+	@Test(expected = Exception.class)
+	public void testNull() throws Exception {
+		Node.getNth(null, 0);
+	}
+
+	@Test(expected = Exception.class)
+	public void testWrongIndex() throws Exception {
+		Node.getNth(new Node(), 1);
 	}
 
 	@Test
-	public void testWrongIndex() {
-		try {
-			Node.getNth(new Node(), 1);
-			assertTrue(false);
-		} catch (Exception e) {
-			assertTrue(true);
-		}
-	}
-
-	@Test
-	public void test2Nodes() {
+	public void test2Nodes() throws Exception {
 		Node n = new Node();
 		n.data = 1337;
 		n.next = new Node();
 		n.next.data = 42;
 		n.next.next = new Node();
 		n.next.next.data = 23;
-		try {
-			assertEquals(Node.getNth(n, 0), 1337);
-			assertEquals(Node.getNth(n, 1), 42);
-			assertEquals(Node.getNth(n, 2), 23);
-		} catch (Exception e) {
-			assertTrue(false);
-		}
+		
+		assertEquals(Node.getNth(n, 0), 1337);
+		assertEquals(Node.getNth(n, 1), 42);
+		assertEquals(Node.getNth(n, 2), 23);
 	}
 
 }
