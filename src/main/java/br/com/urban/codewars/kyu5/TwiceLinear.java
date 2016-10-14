ce4ab388236da8ab13b434fd58816a068963cbc2
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-10-13 22:14:24 -0300

    outra abordagem

--- src/main/java/br/com/urban/codewars/kyu5/TwiceLinear.java
+++ src/main/java/br/com/urban/codewars/kyu5/TwiceLinear.java
@@ -1,7 +1,10 @@
 package br.com.urban.codewars.kyu5;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 
 /*
  * https://www.codewars.com/kata/twice-linear
@@ -9,20 +12,34 @@ import java.util.TreeSet;
 public class TwiceLinear {
 
 	public static int dblLinear(int n) {
-		Set<Integer> set = new TreeSet<>();
+		Set<Integer> set = new HashSet<Integer>();
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(1);
 		set.add(1);
-		for(int i = 0; i < n; i++) {
-			int x = (int) set.toArray()[i];
-			set.add(y(x));
-			set.add(z(x));
+		for (int i = 0; i <= n * 4; i++) {
+			int x = list.get(i);
+			int y = y(x);
+			int z = z(x);
+
+			if (!set.contains(y)) {
+				set.add(y);
+				list.add(y);
+			}
+
+			if (!set.contains(z)) {
+				set.add(z);
+				list.add(z);
+			}
 		}
-		return (int)set.toArray()[n];
+		Collections.sort(list);
+		System.out.println(list);
+		return list.get(n);
 	}
-	
+
 	private static final int y(int x) {
 		return 2 * x + 1;
 	}
-	
+
 	private static final int z(int x) {
 		return 3 * x + 1;
 	}
--- src/test/java/br/com/urban/codewars/kyu5/TwiceLinearTest.java
+++ src/test/java/br/com/urban/codewars/kyu5/TwiceLinearTest.java
@@ -12,12 +12,17 @@ public class TwiceLinearTest {
 	}
 	
 	@Test
-	public void teste() {
+	public void basicTestes() {
 		assertEquals(22, TwiceLinear.dblLinear(10));
 		assertEquals(57, TwiceLinear.dblLinear(20));
 		assertEquals(91, TwiceLinear.dblLinear(30));
 		assertEquals(175, TwiceLinear.dblLinear(50));
-		//assertEquals(80914, TwiceLinear.dblLinear(6000));
-		//assertEquals(1511311, TwiceLinear.dblLinear(60000)); // Process was terminated. It took longer than 11000ms to complete
+	}
+	
+	@Test
+	public void advancedTestes() {
+		assertEquals(447, TwiceLinear.dblLinear(100));
+		assertEquals(80914, TwiceLinear.dblLinear(6000));
+		assertEquals(1511311, TwiceLinear.dblLinear(60000)); // Process was terminated. It took longer than 11000ms to complete
 	}
 }
