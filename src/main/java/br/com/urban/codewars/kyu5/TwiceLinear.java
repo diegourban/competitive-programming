b0b3f458cc72f4178b315fb6fd9551927e014ae4
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-10-14 08:30:39 -0300

    simple approach with sortedset and iterations
    performance ftw

--- src/main/java/br/com/urban/codewars/kyu5/TwiceLinear.java
+++ src/main/java/br/com/urban/codewars/kyu5/TwiceLinear.java
@@ -1,47 +1,37 @@
 package br.com.urban.codewars.kyu5;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /*
  * https://www.codewars.com/kata/twice-linear
  */
 public class TwiceLinear {
 
-	public static int dblLinear(int n) {
-		Set<Integer> set = new HashSet<Integer>();
-		List<Integer> list = new ArrayList<Integer>();
-		list.add(1);
-		set.add(1);
-		for (int i = 0; i <= n * 4; i++) {
-			int x = list.get(i);
-			int y = y(x);
-			int z = z(x);
+    public static int dblLinear(int n) {
+        SortedSet<Integer> set = new TreeSet<>();
+        set.add(1);
 
-			if (!set.contains(y)) {
-				set.add(y);
-				list.add(y);
-			}
+        int iterations = 0;
 
-			if (!set.contains(z)) {
-				set.add(z);
-				list.add(z);
-			}
-		}
-		Collections.sort(list);
-		System.out.println(list);
-		return list.get(n);
-	}
+        while (true) {
+            int min = set.first();
+            set.remove(min);
+            set.add(fy(min));
+            set.add(fz(min));
 
-	private static final int y(int x) {
-		return 2 * x + 1;
-	}
+            iterations++;
+            if (iterations == n + 1) {
+                return min;
+            }
+        }
+    }
 
-	private static final int z(int x) {
-		return 3 * x + 1;
-	}
+    private static final int fy(int x) {
+        return 2 * x + 1;
+    }
 
+    private static final int fz(int x) {
+        return 3 * x + 1;
+    }
 }
