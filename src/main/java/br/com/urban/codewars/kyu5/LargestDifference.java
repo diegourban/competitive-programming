ef7a987ac3200fa36f8fdfd176f06e745c7bcd25
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-10-18 12:55:13 -0200

    Largest difference

--- src/main/java/br/com/urban/codewars/kyu5/LargestDifference.java
+++ src/main/java/br/com/urban/codewars/kyu5/LargestDifference.java
@@ -1,5 +1,8 @@
 package br.com.urban.codewars.kyu5;
 
+/*
+* https://www.codewars.com/kata/largest-difference-in-increasing-indexes
+*/
 public class LargestDifference {
 
 	/*
@@ -24,8 +27,19 @@ public class LargestDifference {
 	 */
 
 	public static int largestDifference(int[] data) {
-		// code here
-		return 0;
-	}
+		int largestDifference = 0;
+
+		for (int i = 0; i < data.length; i++) {
+		    for (int j = data.length - 1; j > i; j--) {
+			if (data[j] >= data[i]) {
+			    int difference = j - i;
+			    if (difference > largestDifference) {
+				largestDifference = difference;
+			    }
+			}
+		    }
+		}
+		return largestDifference;
+	    }
 
 }
