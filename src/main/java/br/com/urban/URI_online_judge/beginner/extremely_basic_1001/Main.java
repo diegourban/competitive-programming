51c9e4f720f42d2208151e44acff5f7d37963735
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-10-20 20:49:13 -0200

    1001

---
+++ src/main/java/br/com/urban/URI_online_judge/beginner/extremely_basic_1001/Main.java
@@ -0,0 +1,25 @@
+package br.com.urban.URI_online_judge.beginner.extremely_basic_1001;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+/**
+ * https://www.urionlinejudge.com.br/judge/en/problems/view/1001
+ * 
+ * @author Diego
+ *
+ */
+public class Main {
+
+	private static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	private static final PrintStream out = System.out;
+
+	public static void main(String[] args) throws IOException {
+		int A = Integer.parseInt(in.readLine());
+		int B = Integer.parseInt(in.readLine());
+		out.printf("X = %d\n", A + B);
+	}
+
+}
