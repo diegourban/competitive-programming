e2b88a393c59c2d33bc043417910f12e64d024ae
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-10-28 13:03:11 -0200

    Adjusting the remove of line separator

--- src/test/java/urban/uri_online_judge/InputStreamBuilder.java
+++ src/test/java/urban/uri_online_judge/InputStreamBuilder.java
@@ -5,20 +5,20 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 public class InputStreamBuilder {
-	
+
 	private StringBuilder builder;
-	
+
 	public InputStreamBuilder() {
 		this.builder = new StringBuilder();
 	}
-	
+
 	public InputStreamBuilder addLine(String str) {
 		this.builder = this.builder.append(str).append(System.lineSeparator());
 		return this;
 	}
-	
+
 	public InputStream build() {
-		this.builder = this.builder.deleteCharAt(this.builder.length() - 1);
+		this.builder = this.builder.deleteCharAt(this.builder.length() - System.lineSeparator().length());
 		return new ByteArrayInputStream(this.builder.toString().getBytes(StandardCharsets.UTF_8));
 	}
 
