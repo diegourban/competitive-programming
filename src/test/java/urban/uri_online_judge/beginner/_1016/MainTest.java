b54c2bfc246ee4131befe1bad2d61bf15a0b34ac
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-10-29 12:21:13 -0200

    1016 distance

---
+++ src/test/java/urban/uri_online_judge/beginner/_1016/MainTest.java
@@ -0,0 +1,69 @@
+package urban.uri_online_judge.beginner._1016;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import urban.uri_online_judge.InputStreamBuilder;
+import urban.uri_online_judge.SampleBuilder;
+import urban.uri_online_judge.beginner._1016.Main;
+
+public class MainTest {
+	
+	private InputStreamBuilder inputStream;
+
+	@Before
+	public void beforeTest() {
+		inputStream = new InputStreamBuilder();
+	}
+
+	@After
+	public void afterTest() {
+		inputStream = null;
+	}
+
+	@Test
+	public void shouldCreateInstance() {
+		assertNotNull(new Main());
+	}
+	
+	@Test
+	public void shouldCalculate60minutes() throws IOException {
+		String inputSample = new SampleBuilder().append(30).build();
+		InputStream input = this.inputStream.build(inputSample);
+		
+		String output = Main.main(input);
+		
+		String expectedOutput = new SampleBuilder().appendln("60 minutos").build();
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldCalculate220minutes() throws IOException {
+		String inputSample = new SampleBuilder().append(110).build();
+		InputStream input = this.inputStream.build(inputSample);
+		
+		String output = Main.main(input);
+		
+		String expectedOutput = new SampleBuilder().appendln("220 minutos").build();
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldCalculate14minutes() throws IOException {
+		String inputSample = new SampleBuilder().append(7).build();
+		InputStream input = this.inputStream.build(inputSample);
+		
+		String output = Main.main(input);
+		
+		String expectedOutput = new SampleBuilder().appendln("14 minutos").build();
+		assertEquals(expectedOutput, output);
+	}
+
+}
