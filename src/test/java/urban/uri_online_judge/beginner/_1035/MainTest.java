3439f6769303826e581ff3f01906df0964e9806b
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-11-01 12:33:31 -0200

    1035 Selection test 1

---
+++ src/test/java/urban/uri_online_judge/beginner/_1035/MainTest.java
@@ -0,0 +1,144 @@
+package urban.uri_online_judge.beginner._1035;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import urban.uri_online_judge.InputStreamBuilder;
+import urban.uri_online_judge.SampleBuilder;
+import urban.uri_online_judge.beginner._1035.Main;
+
+public class MainTest {
+
+	private InputStreamBuilder inputStream;
+
+	@Before
+	public void beforeTest() {
+		inputStream = new InputStreamBuilder();
+	}
+
+	@After
+	public void afterTest() {
+		inputStream = null;
+	}
+
+	@Test
+	public void shouldCreateInstance() {
+		assertNotNull(new Main());
+	}
+
+	@Test
+	public void shouldValidateSelectionMinimalAcceptanceInput() throws IOException {
+		String inputSample = new SampleBuilder().append(0).append(2).append(1).append(2).build();
+		InputStream input = this.inputStream.build(inputSample);
+		
+		String output = Main.main(input);
+		
+		String expectedOutput = new SampleBuilder().appendln("Valores aceitos").build();
+		
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldValidateSelectionTestBranch1() throws IOException {
+		String inputSample = new SampleBuilder().append(0).append(1).append(1).append(2).build();
+		InputStream input = this.inputStream.build(inputSample);
+		
+		String output = Main.main(input);
+		
+		String expectedOutput = new SampleBuilder().appendln("Valores nao aceitos").build();
+		
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldValidateSelectionTestBranch2() throws IOException {
+		String inputSample = new SampleBuilder().append(0).append(2).append(1).append(0).build();
+		InputStream input = this.inputStream.build(inputSample);
+		
+		String output = Main.main(input);
+		
+		String expectedOutput = new SampleBuilder().appendln("Valores nao aceitos").build();
+		
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldValidateSelectionTestBranch3() throws IOException {
+		String inputSample = new SampleBuilder().append(2).append(2).append(1).append(2).build();
+		InputStream input = this.inputStream.build(inputSample);
+		
+		String output = Main.main(input);
+		
+		String expectedOutput = new SampleBuilder().appendln("Valores nao aceitos").build();
+		
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldValidateSelectionTestBranch4() throws IOException {
+		String inputSample = new SampleBuilder().append(0).append(2).append(-1).append(2).build();
+		InputStream input = this.inputStream.build(inputSample);
+		
+		String output = Main.main(input);
+		
+		String expectedOutput = new SampleBuilder().appendln("Valores nao aceitos").build();
+		
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldValidateSelectionTestBranch5() throws IOException {
+		String inputSample = new SampleBuilder().append(0).append(2).append(1).append(-2).build();
+		InputStream input = this.inputStream.build(inputSample);
+		
+		String output = Main.main(input);
+		
+		String expectedOutput = new SampleBuilder().appendln("Valores nao aceitos").build();
+		
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldValidateSelectionTestBranch6() throws IOException {
+		String inputSample = new SampleBuilder().append(1).append(2).append(1).append(2).build();
+		InputStream input = this.inputStream.build(inputSample);
+		
+		String output = Main.main(input);
+		
+		String expectedOutput = new SampleBuilder().appendln("Valores nao aceitos").build();
+		
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldValidateSelectionTestSample1() throws IOException {
+		String inputSample = new SampleBuilder().append(5).append(6).append(7).append(8).build();
+		InputStream input = this.inputStream.build(inputSample);
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln("Valores nao aceitos").build();
+
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldValidateSelectionTestSample2() throws IOException {
+		String inputSample = new SampleBuilder().append(2).append(3).append(2).append(6).build();
+		InputStream input = this.inputStream.build(inputSample);
+		
+		String output = Main.main(input);
+		
+		String expectedOutput = new SampleBuilder().appendln("Valores aceitos").build();
+		
+		assertEquals(expectedOutput, output);
+	}
+
+}
