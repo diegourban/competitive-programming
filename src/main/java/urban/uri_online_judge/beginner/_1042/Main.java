d0b720fbc7e6d03d7c89bc78a334e4b5c7cf698f
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-11-02 13:03:28 -0200

    1042 simple sort

---
+++ src/main/java/urban/uri_online_judge/beginner/_1042/Main.java
@@ -0,0 +1,79 @@
+package urban.uri_online_judge.beginner._1042;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import urban.uri_online_judge.Reader;
+
+/**
+ * Simple Sort
+ * 
+ * https://www.urionlinejudge.com.br/judge/en/problems/view/1042
+ * 
+ * @author Diego Leonardo Urban
+ *
+ */
+public class Main {
+
+	private static final String LS = System.lineSeparator();
+
+	public static void main(String[] args) throws IOException {
+		System.out.print(main(System.in));
+	}
+
+	protected static String main(InputStream input) throws IOException {
+		Reader reader = new Reader(input);
+		int a = reader.nextInt();
+		int b = reader.nextInt();
+		int c = reader.nextInt();
+		reader.close();
+
+		int min = 0;
+		int max = 0;
+		int med = 0;
+
+		if (a > b) {
+			if (a > c) {
+				max = a;
+				if (b > c) {
+					med = b;
+					min = c;
+				} else {
+					med = c;
+					min = b;
+				}
+			} else {
+				med = a;
+				if (b > c) {
+					max = b;
+					min = c;
+				} else {
+					max = c;
+					min = b;
+				}
+			}
+		} else {
+			if (b > c) {
+				max = b;
+				if (a > c) {
+					med = a;
+					min = c;
+				} else {
+					med = c;
+					min = a;
+				}
+			} else {
+				med = b;
+				max = c;
+				min = a;
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		sb = sb.append(min).append(LS).append(med).append(LS).append(max).append(LS).append(LS);
+		sb = sb.append(a).append(LS).append(b).append(LS).append(c).append(LS);
+
+		return sb.toString();
+	}
+
+}
---
+++ src/test/java/urban/uri_online_judge/beginner/_1042/MainTest.java
@@ -0,0 +1,113 @@
+package urban.uri_online_judge.beginner._1042;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import urban.uri_online_judge.InputStreamBuilder;
+import urban.uri_online_judge.SampleBuilder;
+
+public class MainTest {
+
+	private InputStreamBuilder inputStream;
+
+	@Before
+	public void beforeTest() {
+		inputStream = new InputStreamBuilder();
+	}
+
+	@After
+	public void afterTest() {
+		inputStream = null;
+	}
+
+	@Test
+	public void shouldCreateInstance() {
+		assertNotNull(new Main());
+	}
+
+	@Test
+	public void shouldHandleAlreadySortedValues() throws IOException {
+		String inputSample = new SampleBuilder().append(1).append(2).append(3).build();
+		InputStream input = this.inputStream.build(inputSample);
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln(1).appendln(2).appendln(3).appendln("").appendln(1)
+				.appendln(2).appendln(3).build();
+
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void shouldHandleReversedOrder() throws IOException {
+		String inputSample = new SampleBuilder().append(3).append(2).append(1).build();
+		InputStream input = this.inputStream.build(inputSample);
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln(1).appendln(2).appendln(3).appendln("").appendln(3)
+				.appendln(2).appendln(1).build();
+
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void shouldHandleNearlySorted() throws IOException {
+		String inputSample = new SampleBuilder().append(1).append(3).append(2).build();
+		InputStream input = this.inputStream.build(inputSample);
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln(1).appendln(2).appendln(3).appendln("").appendln(1)
+				.appendln(3).appendln(2).build();
+
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void shouldHandleNearlyEqualValues() throws IOException {
+		String inputSample = new SampleBuilder().append(3).append(2).append(3).build();
+		InputStream input = this.inputStream.build(inputSample);
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln(2).appendln(3).appendln(3).appendln("").appendln(3)
+				.appendln(2).appendln(3).build();
+
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void shouldSortSample1() throws IOException {
+		String inputSample = new SampleBuilder().append(7).append(21).append(-14).build();
+		InputStream input = this.inputStream.build(inputSample);
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln(-14).appendln(7).appendln(21).appendln("").appendln(7)
+				.appendln(21).appendln(-14).build();
+
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void shouldSortSample2() throws IOException {
+		String inputSample = new SampleBuilder().append(-14).append(21).append(7).build();
+		InputStream input = this.inputStream.build(inputSample);
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln(-14).appendln(7).appendln(21).appendln("").appendln(-14)
+				.appendln(21).appendln(7).build();
+
+		assertEquals(expectedOutput, output);
+	}
+
+}
