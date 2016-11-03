7f51a7a32a9b0c1317325cad11b5c746762d0a80
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-11-03 12:21:33 -0200

    1044 Multiples

---
+++ src/main/java/urban/uri_online_judge/beginner/_1044/Main.java
@@ -0,0 +1,41 @@
+package urban.uri_online_judge.beginner._1044;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import urban.uri_online_judge.Reader;
+
+/**
+ * Multiples
+ * 
+ * https://www.urionlinejudge.com.br/judge/en/problems/view/1044
+ * 
+ * @author Diego Leonardo Urban
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		System.out.print(main(System.in));
+	}
+
+	protected static String main(InputStream input) throws IOException {
+		Reader reader = new Reader(input);
+		int A = reader.nextInt();
+		int B = reader.nextInt();
+		reader.close();
+
+		if (A >= B) {
+			if (A % B == 0) {
+				return "Sao Multiplos" + System.lineSeparator();
+			}
+		} else {
+			if (B % A == 0) {
+				return "Sao Multiplos" + System.lineSeparator();
+			}
+		}
+
+		return "Nao sao Multiplos" + System.lineSeparator();
+	}
+
+}
