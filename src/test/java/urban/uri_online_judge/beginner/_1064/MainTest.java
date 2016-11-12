0fa3a8631c02c3942f2345d15276f748c9d0bf6b
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-11-12 17:49:01 -0200

    1064 positives and averages

---
+++ src/test/java/urban/uri_online_judge/beginner/_1064/MainTest.java
@@ -0,0 +1,96 @@
+package urban.uri_online_judge.beginner._1064;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import urban.uri_online_judge.InputStreamBuilder;
+import urban.uri_online_judge.SampleBuilder;
+import urban.uri_online_judge.beginner._1064.Main;
+
+public class MainTest {
+	
+	private InputStreamBuilder inputStream;
+
+	@Before
+	public void beforeTest() {
+		inputStream = new InputStreamBuilder();
+	}
+
+	@After
+	public void afterTest() {
+		inputStream = null;
+	}
+
+	@Test
+	public void shouldCreateInstance() {
+		assertNotNull(new Main());
+	}
+	
+	@Test
+	public void shouldFindOnePositive() throws IOException {
+		String inputSample = new SampleBuilder().append(-1.1).append(-3).append(-2.9).append(4).append(-5).append(-0.1).build();
+		InputStream input = this.inputStream.build(inputSample);
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln("1 valores positivos").appendln(4.0).build();
+
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldFindFivePositives() throws IOException {
+		String inputSample = new SampleBuilder().append(1.1).append(-3).append(2.9).append(4).append(5).append(0.1).build();
+		InputStream input = this.inputStream.build(inputSample);
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln("5 valores positivos").appendln(2.6).build();
+
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldFindAllPositives() throws IOException {
+		String inputSample = new SampleBuilder().append(1.1).append(3).append(2.9).append(4).append(5).append(0.1).build();
+		InputStream input = this.inputStream.build(inputSample);
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln("6 valores positivos").appendln(2.7).build();
+
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldConsiderZeroAsPositive() throws IOException {
+		String inputSample = new SampleBuilder().append(1.1).append(-3).append(22.9).append(4).append(-5).append(0).build();
+		InputStream input = this.inputStream.build(inputSample);
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln("4 valores positivos").appendln(7.0).build();
+
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldFindPositivesSample() throws IOException {
+		String inputSample = new SampleBuilder().append(7).append(-5).append(6).append(-3.4).append(4.6).append(12).build();
+		InputStream input = this.inputStream.build(inputSample);
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln("4 valores positivos").appendln(7.4).build();
+
+		assertEquals(expectedOutput, output);
+	}
+
+}
