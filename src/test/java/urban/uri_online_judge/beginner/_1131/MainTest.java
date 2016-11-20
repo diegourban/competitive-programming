b5f68d2e1f92f7644c215f30736d85fa2eead5c3
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-11-20 18:29:50 -0200

    1131 Grenais

---
+++ src/test/java/urban/uri_online_judge/beginner/_1131/MainTest.java
@@ -0,0 +1,122 @@
+package urban.uri_online_judge.beginner._1131;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import urban.uri_online_judge.InputStreamBuilder;
+import urban.uri_online_judge.SampleBuilder;
+import urban.uri_online_judge.beginner._1131.Main;
+
+public class MainTest {
+	
+	private InputStreamBuilder inputStream;
+
+	@Before
+	public void beforeTest() {
+		inputStream = new InputStreamBuilder();
+	}
+
+	@After
+	public void afterTest() {
+		inputStream = null;
+	}
+
+	@Test
+	public void shouldCreateInstance() {
+		assertNotNull(new Main());
+	}
+	
+	@Test
+	public void shouldHandleDraw() throws IOException {
+		String inputSample = new SampleBuilder()//
+				.appendln("1 1")//
+				.appendln(2)//
+				.build();
+		InputStream input = this.inputStream.build(inputSample);
+
+		String output = Main.main(input);
+		String expectedOutput = new SampleBuilder()//
+				.appendln("Novo grenal (1-sim 2-nao)")//
+				.appendln("1 grenais")//
+				.appendln("Inter:0")//
+				.appendln("Gremio:0")//
+				.appendln("Empates:1")//
+				.appendln("Não houve vencedor").build();
+
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldHandleInterWin() throws IOException {
+		String inputSample = new SampleBuilder()//
+				.appendln("2 1")//
+				.appendln(2)//
+				.build();
+		InputStream input = this.inputStream.build(inputSample);
+
+		String output = Main.main(input);
+		String expectedOutput = new SampleBuilder()//
+				.appendln("Novo grenal (1-sim 2-nao)")//
+				.appendln("1 grenais")//
+				.appendln("Inter:1")//
+				.appendln("Gremio:0")//
+				.appendln("Empates:0")//
+				.appendln("Inter venceu mais").build();
+
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldHandleGremioWin() throws IOException {
+		String inputSample = new SampleBuilder()//
+				.appendln("1 2")//
+				.appendln(2)//
+				.build();
+		InputStream input = this.inputStream.build(inputSample);
+
+		String output = Main.main(input);
+		String expectedOutput = new SampleBuilder()//
+				.appendln("Novo grenal (1-sim 2-nao)")//
+				.appendln("1 grenais")//
+				.appendln("Inter:0")//
+				.appendln("Gremio:1")//
+				.appendln("Empates:0")//
+				.appendln("Gremio venceu mais").build();
+
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldValidateSample() throws IOException {
+		String inputSample = new SampleBuilder()//
+				.appendln("3 2")//
+				.appendln(1)//
+				.appendln("2 3")//
+				.appendln(1)//
+				.appendln("3 1")//
+				.append(2)//
+				.build();
+		InputStream input = this.inputStream.build(inputSample);
+
+		String output = Main.main(input);
+		String expectedOutput = new SampleBuilder()//
+				.appendln("Novo grenal (1-sim 2-nao)")//
+				.appendln("Novo grenal (1-sim 2-nao)")//
+				.appendln("Novo grenal (1-sim 2-nao)")//
+				.appendln("3 grenais")//
+				.appendln("Inter:2")//
+				.appendln("Gremio:1")//
+				.appendln("Empates:0")//
+				.appendln("Inter venceu mais").build();
+
+		assertEquals(expectedOutput, output);
+	}
+
+}
