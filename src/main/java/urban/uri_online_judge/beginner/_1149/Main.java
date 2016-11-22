b201a20c4f0c99027ec4f8cecef3559711ee8f0c
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-11-22 12:43:20 -0200

    1149 Summing Consecutive Integers

---
+++ src/main/java/urban/uri_online_judge/beginner/_1149/Main.java
@@ -0,0 +1,44 @@
+package urban.uri_online_judge.beginner._1149;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import urban.uri_online_judge.Reader;
+
+/**
+ * Summing Consecutive Integers
+ * 
+ * https://www.urionlinejudge.com.br/judge/en/problems/view/1149
+ * 
+ * @author Diego Leonardo Urban
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		System.out.print(main(System.in));
+	}
+
+	protected static String main(InputStream input) throws IOException {
+		Reader reader = new Reader(input);
+
+		int a = 0;
+		int n = 0;
+		int sum = 0;
+
+		a = reader.nextInt();
+		n = reader.nextInt();
+
+		while (n < 1) {
+			n = reader.nextInt();
+		}
+		reader.close();
+
+		for (int i = 0; i < n; i++, a++) {
+			sum += a;
+		}
+
+		return sum + System.lineSeparator();
+	}
+
+}
