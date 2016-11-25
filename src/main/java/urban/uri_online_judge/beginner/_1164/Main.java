ff8da226412d1d5c5f92187a6ae942f30db45588
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-11-25 12:31:21 -0200

    1164 Perfect Number

---
+++ src/main/java/urban/uri_online_judge/beginner/_1164/Main.java
@@ -0,0 +1,57 @@
+package urban.uri_online_judge.beginner._1164;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import urban.uri_online_judge.Reader;
+
+/**
+ * Perfect Number
+ * 
+ * https://www.urionlinejudge.com.br/judge/en/problems/view/1164
+ * 
+ * @author Diego Leonardo Urban
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		System.out.print(main(System.in));
+	}
+
+	protected static String main(InputStream input) throws IOException {
+		Reader reader = new Reader(input);
+		int n = reader.nextInt();
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 1; i <= n; i++) {
+			int x = reader.nextInt();
+
+			int sum = divisorsSum(x);
+
+			if (sum == x) {
+				sb.append(x).append(" eh perfeito");
+			} else {
+				sb.append(x).append(" nao eh perfeito");
+			}
+			sb.append(System.lineSeparator());
+		}
+		reader.close();
+
+		return sb.toString();
+	}
+
+	private static int divisorsSum(int x) {
+		if (x == 1)
+			return 0;
+		int dSum = 1;
+		for (int j = 2; j <= x / 2; j++) {
+			if (x % j == 0) {
+				dSum += j;
+			}
+		}
+		return dSum;
+	}
+
+}
