e26322d02313e4afada6776b7de22e63f26c3e97
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-11-30 19:59:16 -0200

    1184 Below the Main Diagonal

--- src/test/java/urban/uri_online_judge/beginner/_1183/MainTest.java
+++ src/test/java/urban/uri_online_judge/beginner/_1183/MainTest.java
@@ -10,17 +10,16 @@ import org.junit.Test;
 
 import urban.uri_online_judge.SampleBuilder;
 import urban.uri_online_judge.SampleContentLoader;
-import urban.uri_online_judge.beginner._1183.Main;
 
 public class MainTest {
-	
+
 	private SampleContentLoader scl = new SampleContentLoader("beginner", "1183");
 
 	@Test
 	public void shouldCreateInstance() {
 		assertNotNull(new Main());
 	}
-	
+
 	@Test
 	public void shouldHandleSample1() throws IOException {
 		InputStream input = scl.loadAsInputStream("sample1.in");
@@ -31,7 +30,7 @@ public class MainTest {
 
 		assertEquals(expectedOutput, output);
 	}
-	
+
 	@Test
 	public void shouldHandleSample2() throws IOException {
 		InputStream input = scl.loadAsInputStream("sample2.in");
@@ -42,7 +41,7 @@ public class MainTest {
 
 		assertEquals(expectedOutput, output);
 	}
-	
+
 	@Test
 	public void shouldHandleSample3() throws IOException {
 		InputStream input = scl.loadAsInputStream("sample3.in");
@@ -53,29 +52,27 @@ public class MainTest {
 
 		assertEquals(expectedOutput, output);
 	}
-	
+
 	@Test
 	public void shouldHandleSample4() throws IOException {
 		InputStream input = scl.loadAsInputStream("sample4.in");
-		
+
 		String output = Main.main(input);
-		
+
 		String expectedOutput = new SampleBuilder().appendln(7.9).build();
-		
+
 		assertEquals(expectedOutput, output);
 	}
-	
+
 	@Test
 	public void shouldHandleSample5() throws IOException {
 		InputStream input = scl.loadAsInputStream("sample5.in");
-		
+
 		String output = Main.main(input);
-		
+
 		String expectedOutput = new SampleBuilder().appendln(0.0).build();
-		
+
 		assertEquals(expectedOutput, output);
 	}
-	
-
 
 }
