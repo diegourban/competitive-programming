61ad4b801a56eb2a8a339a1b1277cb43e1a4d8e4
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-11-30 12:24:47 -0200

    1183 Above the Main Diagonal

---
+++ src/main/java/urban/uri_online_judge/beginner/_1183/Main.java
@@ -0,0 +1,57 @@
+package urban.uri_online_judge.beginner._1183;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+
+import urban.uri_online_judge.Reader;
+
+/**
+ * Above the Main Diagonal
+ * 
+ * https://www.urionlinejudge.com.br/judge/en/problems/view/1183
+ * 
+ * @author Diego Leonardo Urban
+ *
+ */
+public class Main {
+
+	private static final NumberFormat nf;
+
+	static {
+		DecimalFormatSymbols dfs = DecimalFormatSymbols.getInstance();
+		dfs.setDecimalSeparator('.');
+		nf = new DecimalFormat("#0.0", dfs);
+	}
+
+	public static void main(String[] args) throws IOException {
+		System.out.print(main(System.in));
+	}
+
+	protected static String main(InputStream input) throws IOException {
+		Reader reader = new Reader(input);
+		char op = reader.nextChar();
+
+		double[][] array = new double[12][12];
+		double sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				double x = reader.nextDouble();
+				array[i][j] = x;
+				if (j > i) {
+					sum += x;
+				}
+			}
+		}
+		reader.close();
+
+		if (op == 'M') {
+			return nf.format(sum / 66.0) + System.lineSeparator();
+		}
+
+		return nf.format(sum) + System.lineSeparator();
+	}
+
+}
