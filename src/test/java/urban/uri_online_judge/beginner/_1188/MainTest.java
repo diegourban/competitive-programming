f1584dc2017424295824b11f6ebe7bd72ddcce9c
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-12-02 19:01:31 -0200

    1188 Inferior Area

---
+++ src/test/java/urban/uri_online_judge/beginner/_1188/MainTest.java
@@ -0,0 +1,90 @@
+package urban.uri_online_judge.beginner._1188;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+import urban.uri_online_judge.SampleBuilder;
+import urban.uri_online_judge.SampleContentLoader;
+import urban.uri_online_judge.beginner._1188.Main;
+
+public class MainTest {
+	
+	private SampleContentLoader scl = new SampleContentLoader("beginner", "1188");
+
+	@Test
+	public void shouldCreateInstance() {
+		assertNotNull(new Main());
+	}
+
+	@Test
+	public void shouldHandleSample1() throws IOException {
+		InputStream input = scl.loadAsInputStream("sample1.in");
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln(3675.0).build();
+
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void shouldHandleSample2() throws IOException {
+		InputStream input = scl.loadAsInputStream("sample2.in");
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln(122.5).build();
+
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void shouldHandleSample3() throws IOException {
+		InputStream input = scl.loadAsInputStream("sample3.in");
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln(125.0).build();
+
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void shouldHandleSample4() throws IOException {
+		InputStream input = scl.loadAsInputStream("sample4.in");
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln(4.2).build();
+
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldHandleSample5() throws IOException {
+		InputStream input = scl.loadAsInputStream("sample5.in");
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln(-29.0).build();
+
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldHandleSample6() throws IOException {
+		InputStream input = scl.loadAsInputStream("sample6.in");
+		
+		String output = Main.main(input);
+		
+		String expectedOutput = new SampleBuilder().appendln(-1.0).build();
+		
+		assertEquals(expectedOutput, output);
+	}
+
+}
