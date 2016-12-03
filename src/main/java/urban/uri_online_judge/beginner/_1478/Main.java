bf80e5c90d3864a660e845db1ebe6d0441099872
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-12-03 14:43:46 -0200

    1478 - Square Matrix II

---
+++ src/main/java/urban/uri_online_judge/beginner/_1478/Main.java
@@ -0,0 +1,44 @@
+package urban.uri_online_judge.beginner._1478;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import urban.uri_online_judge.Reader;
+
+/**
+ * Square Matrix II
+ * 
+ * https://www.urionlinejudge.com.br/judge/en/problems/view/1478
+ * 
+ * @author Diego Leonardo Urban
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		System.out.print(main(System.in));
+	}
+
+	protected static String main(InputStream input) throws IOException {
+		Reader reader = new Reader(input);
+		int n = reader.nextInt();
+
+		StringBuilder sb = new StringBuilder();
+		while (n != 0) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					sb.append(String.format("%3s", Math.abs(i - j) + 1));
+					if (j < n - 1) {
+						sb.append(" ");
+					}
+				}
+				sb.append(System.lineSeparator());
+			}
+			sb.append(System.lineSeparator());
+			n = reader.nextInt();
+		}
+
+		return sb.toString();
+	}
+
+}
---
+++ src/test/java/urban/uri_online_judge/beginner/_1478/MainTest.java
@@ -0,0 +1,112 @@
+package urban.uri_online_judge.beginner._1478;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+import urban.uri_online_judge.SampleBuilder;
+import urban.uri_online_judge.SampleContentLoader;
+import urban.uri_online_judge.beginner._1478.Main;
+
+public class MainTest {
+	
+	private SampleContentLoader scl = new SampleContentLoader("beginner", "1478");
+
+	@Test
+	public void shouldCreateInstance() {
+		assertNotNull(new Main());
+	}
+
+	@Test
+	public void shouldHandleSquareMatrixOfSize1() throws IOException {
+		InputStream input = new SampleBuilder().appendln(1).append(0).buildAsInputStream();
+
+		String output = Main.main(input);
+
+		String expectedOutput = new SampleBuilder().appendln("  1").appendln("").build();
+
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldHandleSquareMatrixOfSize2() throws IOException {
+		InputStream input = new SampleBuilder().appendln(2).append(0).buildAsInputStream();
+
+		String output = Main.main(input);
+
+		String expectedOutput = scl.loadAsStringFrom("sample2SquareMatrix.out");
+
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldHandleSquareMatrixOfSize3() throws IOException {
+		InputStream input = new SampleBuilder().appendln(3).append(0).buildAsInputStream();
+
+		String output = Main.main(input);
+
+		String expectedOutput = scl.loadAsStringFrom("sample3SquareMatrix.out");
+
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldHandleSquareMatrixOfSize4() throws IOException {
+		InputStream input = new SampleBuilder().appendln(4).append(0).buildAsInputStream();
+
+		String output = Main.main(input);
+
+		String expectedOutput = scl.loadAsStringFrom("sample4SquareMatrix.out");
+
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldHandleSquareMatrixOfSize5() throws IOException {
+		InputStream input = new SampleBuilder().appendln(5).append(0).buildAsInputStream();
+
+		String output = Main.main(input);
+
+		String expectedOutput = scl.loadAsStringFrom("sample5SquareMatrix.out");
+
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldHandleSquareMatrixOfSize100() throws IOException {
+		InputStream input = new SampleBuilder().appendln(100).append(0).buildAsInputStream();
+
+		String output = Main.main(input);
+
+		String expectedOutput = scl.loadAsStringFrom("sample100SquareMatrix.out");
+
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldHandleSequenceOfMatrixes() throws IOException {
+		InputStream input = scl.loadAsInputStream("sampleSequenceOfMatrixes.in");
+
+		String output = Main.main(input);
+
+		String expectedOutput = scl.loadAsStringFrom("sampleSequenceOfMatrixes.out");
+
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void shouldHandleSequenceOfMatrixes2() throws IOException {
+		InputStream input = scl.loadAsInputStream("sampleSequenceOfMatrixes2.in");
+
+		String output = Main.main(input);
+
+		String expectedOutput = scl.loadAsStringFrom("sampleSequenceOfMatrixes2.out");
+
+		assertEquals(expectedOutput, output);
+	}
+
+}
