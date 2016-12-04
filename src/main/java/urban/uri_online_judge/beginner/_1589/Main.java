a92ec74190bbe1d16911d5ab449d55fa0074432f
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-12-04 11:15:42 -0200

    1589 - Bob Conduit

---
+++ src/main/java/urban/uri_online_judge/beginner/_1589/Main.java
@@ -0,0 +1,38 @@
+package urban.uri_online_judge.beginner._1589;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import urban.uri_online_judge.Reader;
+
+/**
+ * Bob Conduit
+ * 
+ * https://www.urionlinejudge.com.br/judge/en/problems/view/1589
+ * 
+ * @author Diego Leonardo Urban
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		System.out.print(main(System.in));
+	}
+
+	protected static String main(InputStream input) throws IOException {
+		Reader reader = new Reader(input);
+		int t = reader.nextInt();
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < t; i++) {
+			int r1 = reader.nextInt();
+			int r2 = reader.nextInt();
+			sb.append(r1 + r2).append(System.lineSeparator());
+		}
+		reader.close();
+
+		return sb.toString();
+	}
+
+}
