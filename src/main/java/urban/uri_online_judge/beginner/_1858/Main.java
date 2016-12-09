11741fabd9dd5b9b2011c56d171f8e510bacf4c2
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-12-09 12:32:21 -0200

    1858 - Theon's Answer

---
+++ src/main/java/urban/uri_online_judge/beginner/_1858/Main.java
@@ -0,0 +1,40 @@
+package urban.uri_online_judge.beginner._1858;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import urban.uri_online_judge.Reader;
+
+/**
+ * Theon's Answer
+ * 
+ * https://www.urionlinejudge.com.br/judge/en/problems/view/1858
+ * 
+ * @author Diego Leonardo Urban
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		System.out.print(main(System.in));
+	}
+
+	protected static String main(InputStream input) throws IOException {
+		Reader reader = new Reader(input);
+
+		int n = reader.nextInt();
+		int min = Integer.MAX_VALUE;
+		int pn = 0;
+		for (int i = 1; i <= n; i++) {
+			int x = reader.nextInt();
+			if (x < min) {
+				min = x;
+				pn = i;
+			}
+		}
+		reader.close();
+
+		return pn + System.lineSeparator();
+	}
+
+}
