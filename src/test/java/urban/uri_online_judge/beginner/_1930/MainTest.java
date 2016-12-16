d9d2afc12716f8b0d20fc96f40d6f1a9e271ea96
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-12-16 12:15:35 -0200

    1930 - Electrical Outlet - Tests

---
+++ src/test/java/urban/uri_online_judge/beginner/_1930/MainTest.java
@@ -0,0 +1,44 @@
+package urban.uri_online_judge.beginner._1930;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+import urban.uri_online_judge.SampleBuilder;
+import urban.uri_online_judge.SampleContentLoader;
+import urban.uri_online_judge.beginner._1930.Main;
+
+public class MainTest {
+	
+	private SampleContentLoader scl = new SampleContentLoader("beginner", "1930");
+
+	@Test
+	public void shouldCreateInstance() {
+		assertNotNull(new Main());
+	}
+
+	@Test
+	public void shouldHandleURISamples() throws IOException {
+		InputStream input1 = scl.loadAsInputStream("sampleURI1.in");
+		InputStream input2 = scl.loadAsInputStream("sampleURI2.in");
+		InputStream input3 = scl.loadAsInputStream("sampleURI3.in");
+
+		String output1 = Main.main(input1);
+		String output2 = Main.main(input2);
+		String output3 = Main.main(input3);
+
+		String expectedOutput1 = new SampleBuilder().appendln("8").build();
+		String expectedOutput2 = new SampleBuilder().appendln("21").build();
+		String expectedOutput3 = new SampleBuilder().appendln("5").build();
+
+		assertEquals(expectedOutput1, output1);
+		assertEquals(expectedOutput2, output2);
+		assertEquals(expectedOutput3, output3);
+	}
+
+}
+
