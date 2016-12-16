488a6266f055dad3ab26facfc4d6dd21195b94ef
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-12-16 12:15:12 -0200

    1930 - Electrical Outlet

---
+++ src/main/java/urban/uri_online_judge/beginner/_1930/Main.java
@@ -0,0 +1,34 @@
+package urban.uri_online_judge.beginner._1930;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import urban.uri_online_judge.Reader;
+
+/**
+ * Electrical Outlet
+ * 
+ * https://www.urionlinejudge.com.br/judge/en/problems/view/1930
+ * 
+ * @author Diego Leonardo Urban
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		System.out.print(main(System.in));
+	}
+
+	protected static String main(InputStream input) throws IOException {
+		Reader reader = new Reader(input);
+		int t1 = reader.nextInt();
+		int t2 = reader.nextInt();
+		int t3 = reader.nextInt();
+		int t4 = reader.nextInt();
+		reader.close();
+
+		int d = t1 + t2 + t3 + t4 - 3;
+
+		return d + System.lineSeparator();
+	}
+}
