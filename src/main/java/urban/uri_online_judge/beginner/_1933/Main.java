e646fd86cb607cb7b8d37cb050af18eb1d3aabfc
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-12-17 09:54:01 -0200

    1933 Tri-du

---
+++ src/main/java/urban/uri_online_judge/beginner/_1933/Main.java
@@ -0,0 +1,37 @@
+package urban.uri_online_judge.beginner._1933;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import urban.uri_online_judge.Reader;
+
+/**
+ * Tri-du
+ * 
+ * https://www.urionlinejudge.com.br/judge/en/problems/view/1933
+ * 
+ * @author Diego Leonardo Urban
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		System.out.print(main(System.in));
+	}
+
+	protected static String main(InputStream input) throws IOException {
+		Reader reader = new Reader(input);
+		int a = reader.nextInt();
+		int b = reader.nextInt();
+		reader.close();
+
+		if (a > b) {
+			return a + System.lineSeparator();
+		} else if (b > a) {
+			return b + System.lineSeparator();
+		}
+
+		return a + System.lineSeparator();
+	}
+
+}
