a99ca9273dd1ba6a05d28e8ed64086939e1838f9
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-12-18 09:56:13 -0200

    1959 - Regular Simples Polygons

---
+++ src/main/java/urban/uri_online_judge/beginner/_1959/Main.java
@@ -0,0 +1,32 @@
+package urban.uri_online_judge.beginner._1959;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import urban.uri_online_judge.Reader;
+
+/**
+ * Regular Simple Polygons
+ * 
+ * https://www.urionlinejudge.com.br/judge/en/problems/view/1959
+ * 
+ * @author Diego Leonardo Urban
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		System.out.print(main(System.in));
+	}
+
+	protected static String main(InputStream input) throws IOException {
+		Reader reader = new Reader(input);
+		long n = reader.nextLong();
+		long l = reader.nextLong();
+		reader.close();
+
+		long p = n * l;
+		return p + System.lineSeparator();
+	}
+
+}
