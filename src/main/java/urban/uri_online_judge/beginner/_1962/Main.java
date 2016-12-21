eee802ff7fd53f40f2646631be709727c290afde
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-12-21 12:16:17 -0200

    1962 - A Long, Long Time Ago

---
+++ src/main/java/urban/uri_online_judge/beginner/_1962/Main.java
@@ -0,0 +1,44 @@
+package urban.uri_online_judge.beginner._1962;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import urban.uri_online_judge.Reader;
+
+/**
+ * A Long, Long Time Ago
+ * 
+ * https://www.urionlinejudge.com.br/judge/en/problems/view/1962
+ * 
+ * @author Diego Leonardo Urban
+ *
+ */
+public class Main {
+
+	private static final int THE_YEAR = 2015;
+
+	public static void main(String[] args) throws IOException {
+		System.out.print(main(System.in));
+	}
+
+	protected static String main(InputStream input) throws IOException {
+		Reader reader = new Reader(input);
+		int n = reader.nextInt();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			long t = reader.nextLong();
+			long dif = Math.abs(THE_YEAR - t);
+			if (t >= THE_YEAR) {
+				dif++;
+				sb.append(dif).append(" A.C.");
+			} else {
+				sb.append(dif).append(" D.C.");
+			}
+			sb.append(System.lineSeparator());
+		}
+		reader.close();
+
+		return sb.toString();
+	}
+
+}
