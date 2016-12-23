f5cf005358a4d33f0e16ed8de50c857c7ac2e864
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-12-23 12:23:28 -0200

    1973 - Star Trek

---
+++ src/main/java/urban/uri_online_judge/beginner/_1973/Main.java
@@ -0,0 +1,61 @@
+package urban.uri_online_judge.beginner._1973;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import urban.uri_online_judge.Reader;
+
+/**
+ * Star Trek
+ * 
+ * https://www.urionlinejudge.com.br/judge/en/problems/view/1973
+ * 
+ * @author Diego Leonardo Urban
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		System.out.print(main(System.in));
+	}
+
+	protected static String main(InputStream input) throws IOException {
+		Reader reader = new Reader(input);
+		int n = reader.nextInt();
+		long totalSheeps = 0;
+		long farmsStolen = 0;
+		long stolenOneWay = 0;
+		long stolenTwoWay = 0;
+		boolean foundAnEven = false;
+		for (int i = 0; i < n; i++) {
+			int x = reader.nextInt();
+			totalSheeps += x;
+
+			if (!foundAnEven) {
+
+				boolean isEven = x % 2 == 0;
+
+				if (x > 0) {
+					stolenOneWay++;
+					stolenTwoWay++;
+				}
+				if (x - 1 > 0 && !isEven) {
+					stolenTwoWay++;
+				}
+
+				farmsStolen++;
+
+				if (isEven) {
+					foundAnEven = true;
+				}
+			}
+
+		}
+		reader.close();
+
+		long stolen = foundAnEven ? stolenTwoWay : stolenOneWay;
+
+		return farmsStolen + " " + (totalSheeps - stolen) + System.lineSeparator();
+	}
+
+}
