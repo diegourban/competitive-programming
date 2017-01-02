bfc60fe80af4b8deb2a2bdecd9f122ebcb766695
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-01-02 12:14:52 -0200

    Add files via upload

--- src/main/java/urban/uri_online_judge/Reader.java
+++ src/main/java/urban/uri_online_judge/Reader.java
@@ -23,7 +23,7 @@ public class Reader {
 	}
 
 	public String readLine() throws IOException {
-		byte[] buf = new byte[100]; // line length
+		byte[] buf = new byte[10000]; // line length
 		int cnt = 0, c;
 
 		while ((c = read()) != -1) {
