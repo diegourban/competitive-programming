9933271934bb9108d24a117991786e7339b49650
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-01-04 21:12:56 -0200

    vai quebra

--- common/src/test/java/urban/common/SampleLoaderTest.java
+++ common/src/test/java/urban/common/SampleLoaderTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.InputStream;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class SampleLoaderTest {
@@ -22,9 +23,35 @@ public class SampleLoaderTest {
     }
 
     @Test
-    public void shouldLoadAsString() throws IOException {
+    public void shouldLoadInputAsString() throws IOException {
         SampleLoader sl = new SampleLoader("samples", "test");
         String content = sl.loadAsString("sample.in");
         assertNotNull(content);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("some content").append(System.lineSeparator());
+        sb.append(1).append(System.lineSeparator());
+        sb.append(5.55).append(System.lineSeparator());
+        sb.append(8712367891236l).append(System.lineSeparator());
+        sb.append("y");
+        String expectedContent = sb.toString();
+
+        assertEquals(expectedContent, content);
+    }
+
+    @Test
+    public void shouldLoadOutputAsString() throws IOException {
+        SampleLoader sl = new SampleLoader("samples", "test");
+        String content = sl.loadAsString("sample.out");
+        assertNotNull(content);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(1).append(System.lineSeparator());
+        sb.append("o").append(System.lineSeparator());
+        sb.append(3.14).append(System.lineSeparator());
+        sb.append("pi").append(System.lineSeparator());
+        String expectedContent = sb.toString();
+
+        assertEquals(expectedContent, content);
     }
 }
