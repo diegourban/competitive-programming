ded9a53a81f62344829f70a06b963d16413ecff3
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-01-05 13:02:40 -0200

    2162 - Peaks and Valleys

---
+++ uri-online-judge/src/main/java/urban/uri_online_judge/beginner/_2162/Main.java
@@ -0,0 +1,55 @@
+package urban.uri_online_judge.beginner._2162;
+
+import urban.common.Reader;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Peaks and Valleys
+ * <p>
+ * https://www.urionlinejudge.com.br/judge/en/problems/view/2162
+ *
+ * @author Diego Leonardo Urban
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.print(main(System.in));
+    }
+
+    protected static String main(InputStream input) throws IOException {
+        Reader reader = new Reader(input);
+        int n = reader.nextInt();
+        int h1 = reader.nextInt();
+        int h2 = reader.nextInt();
+        boolean isPeak = false;
+        if (h2 == h1) {
+            return 0 + System.lineSeparator();
+        }
+        if (h2 > h1) {
+            isPeak = true;
+        } else {
+            isPeak = false;
+        }
+        int previousH = h2;
+        for (int i = 2; i < n; i++) {
+            int h = reader.nextInt();
+            if (isPeak) {
+                if (h >= previousH) {
+                    return 0 + System.lineSeparator();
+                }
+            } else {
+                if (h <= previousH) {
+                    return 0 + System.lineSeparator();
+                }
+            }
+            previousH = h;
+            isPeak = !isPeak;
+        }
+        reader.close();
+
+        return 1 + System.lineSeparator();
+    }
+
+}
