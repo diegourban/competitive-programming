c5a0341ab49087a0cdf79cd3a03872951ffafd35
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-01-07 18:44:38 -0200

    testing tri-du

--- uri-online-judge/src/test/java/urban/uri_online_judge/beginner/_1933/MainTest.java
+++ uri-online-judge/src/test/java/urban/uri_online_judge/beginner/_1933/MainTest.java
@@ -3,8 +3,10 @@ package urban.uri_online_judge.beginner._1933;
 import org.junit.Test;
 import urban.common.SampleBuilder;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -35,6 +37,27 @@ public class MainTest {
                     String output = Main.main(input);
                     String expectedOutput = new SampleBuilder().appendln(Math.max(i, j)).build();
                     assertEquals(expectedOutput, output);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void shouldRunThroughMain() throws IOException {
+        for (int i = 1; i <= 13; i++) {
+            for (int j = 1; j <= 13; j++) {
+                if (i != j) {
+                    InputStream input = new SampleBuilder().append(i).append(j).buildAsInputStream();
+                    System.setIn(input);
+
+                    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+                    System.setOut(new PrintStream(outContent));
+
+                    Main.main(new String[]{});
+
+                    String expectedOutput = new SampleBuilder().appendln(Math.max(i, j)).build();
+
+                    assertEquals(expectedOutput, outContent.toString());
                 }
             }
         }
