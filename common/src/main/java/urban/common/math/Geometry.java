fc4c1da46cbe72052d703509581834a3829b7090
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-01-09 19:07:08 -0200

    adding more geometry util methods

--- common/src/main/java/urban/common/math/Geometry.java
+++ common/src/main/java/urban/common/math/Geometry.java
@@ -16,4 +16,45 @@ public class Geometry {
         return Math.hypot(x1 - x2, y1 - y2);
     }
 
+    /**
+     * Calculates the area of a circle from its radius
+     *
+     * @param radius the radius of the circle
+     * @return the area of the circle
+     */
+    public static double areaOfCircleFromRadius(double radius) {
+        return Math.PI * (radius * radius);
+    }
+
+    /**
+     * Calculates the area of a circle from its diameter
+     *
+     * @param diameter the diameter of the circle
+     * @return the area of the circle
+     */
+    public static double areaOfCircleFromDiameter(double diameter) {
+        double radius = radiusOfCircleFromDiameter(diameter);
+        return areaOfCircleFromRadius(radius);
+    }
+
+    /**
+     * Calculates the radius of a circle from its diameter
+     *
+     * @param diameter the diameter of the circle
+     * @return the radius of the circle
+     */
+    public static double radiusOfCircleFromDiameter(double diameter) {
+        return diameter / 2.0;
+    }
+
+    /**
+     * Calculates the radius of a circle from its area
+     *
+     * @param area the area of the circle
+     * @return the radius of the circle
+     */
+    public static double radiusOfCircleFromArea(double area) {
+        return Math.sqrt(area / Math.PI);
+    }
+
 }
--- common/src/test/java/urban/common/math/GeometryTest.java
+++ common/src/test/java/urban/common/math/GeometryTest.java
@@ -17,4 +17,27 @@ public class GeometryTest {
     public void shouldCalculateDistance() {
         assertEquals(5.0, Geometry.distance(-2, 1, 1, 5), 0.1);
     }
+
+    @Test
+    public void shouldCalculateTheAreaOfCircleFromItsRadius() {
+        assertEquals(28.26, Geometry.areaOfCircleFromRadius(3), 0.1);
+    }
+
+    @Test
+    public void shouldCalculateTheAreaOfCircleFromItsDiameter() {
+        assertEquals(50.24, Geometry.areaOfCircleFromDiameter(8), 0.1);
+    }
+
+    @Test
+    public void shouldCalculateTheRadiusOfCircleFromItsDiameter() {
+        assertEquals(1, Geometry.radiusOfCircleFromDiameter(2), 0.1);
+        assertEquals(1.5, Geometry.radiusOfCircleFromDiameter(3), 0.1);
+        assertEquals(2, Geometry.radiusOfCircleFromDiameter(4), 0.1);
+        assertEquals(100, Geometry.radiusOfCircleFromDiameter(200), 0.1);
+    }
+
+    @Test
+    public void shouldCalculateTheRadiusOfCircleFromItsArea() {
+        assertEquals(5, Geometry.radiusOfCircleFromArea(78.5), 0.1);
+    }
 }
