b47787a3d1235da67ca2cee83e3ed97cd1b59433
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-01-23 12:27:26 -0200

    2160 - Name at Form

--- uri-online-judge/src/test/java/urban/uri_online_judge/beginner/_2160/MainTest.java
+++ uri-online-judge/src/test/java/urban/uri_online_judge/beginner/_2160/MainTest.java
@@ -10,7 +10,6 @@ import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
 
 public class MainTest {
 
