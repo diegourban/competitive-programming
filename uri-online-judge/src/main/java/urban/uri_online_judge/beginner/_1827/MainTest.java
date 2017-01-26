d698bc3e222e30fc11b418fc586416a075973ac2
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-01-26 12:47:31 -0200

    1827 - Square Array IV

---
+++ uri-online-judge/src/main/java/urban/uri_online_judge/beginner/_1827/MainTest.java
@@ -0,0 +1,54 @@
+package urban.uri_online_judge.beginner._1827;
+
+import org.junit.Test;
+import urban.common.SampleBuilder;
+import urban.common.SampleLoader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class MainTest {
+
+    private static final SampleLoader sl = new SampleLoader("beginner", "1827");
+
+    @Test
+    public void shouldCreateInstance() {
+        assertNotNull(new Main());
+    }
+
+    @Test
+    public void shouldHandleLowestInput() throws IOException {
+        InputStream input = new SampleBuilder().append(5).buildAsInputStream();
+        String output = Main.main(input);
+        String expectedOutput = sl.loadAsString("lowestInput.out");
+        assertEquals(expectedOutput, output);
+    }
+
+    @Test
+    public void shouldHandleURISample() throws IOException {
+        InputStream input = new SampleBuilder().appendln(5).append(11).buildAsInputStream();
+        String output = Main.main(input);
+        String expectedOutput = sl.loadAsString("uri.out");
+        assertEquals(expectedOutput, output);
+    }
+
+    @Test
+    public void shouldRunThroughMain() throws IOException {
+        InputStream input = new SampleBuilder().appendln(5).append(11).buildAsInputStream();
+        System.setIn(input);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        Main.main(new String[]{});
+
+        String expectedOutput = sl.loadAsString("uri.out");
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+}
