664f74ac6e17f11d3cd7aa9b1b70b8dc3bc92e58
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-01-26 12:48:00 -0200

    1827 - Square Array IV

--- uri-online-judge/src/main/java/urban/uri_online_judge/beginner/_1827/Main.java
+++ uri-online-judge/src/main/java/urban/uri_online_judge/beginner/_1827/Main.java
@@ -2,6 +2,7 @@ package urban.uri_online_judge.beginner._1827;
 
 import urban.common.Reader;
 
+import java.io.DataInputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -31,7 +32,11 @@ public class Main {
                     for (int j = 0; j < n; j++) {
                         if(i >= innerRange && j >= innerRange && i < n - innerRange && j < n - innerRange) {
                             if(n / 2 == i && i == j) {
-                                sb.append(4);
+                                if(n % 2 == 0) {
+                                    sb.append(1);
+                                } else {
+                                    sb.append(4);
+                                }
                             } else {
                                 sb.append(1);
                             }
@@ -57,5 +62,166 @@ public class Main {
 
         return sb.toString();
     }
+
+    public static class Reader {
+
+        private final int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader(InputStream input) {
+            din = new DataInputStream(input);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[10000]; // line length
+            int cnt = 0;
+            int c = 0;
+
+            while ((c = read()) != -1) {
+                if (c == '\n' || c == '\r') {
+                    if (cnt == 0) {
+                        continue;
+                    }
+                    break;
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+
+            while (c <= ' ') {
+                c = read();
+            }
+
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) {
+                return -ret;
+            }
+
+            return ret;
+        }
+
+        public char nextChar() throws IOException {
+            byte c = read();
+
+            while (c <= ' ') {
+                c = read();
+            }
+
+            return (char) c;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+
+            while (c <= ' ') {
+                c = read();
+            }
+
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) {
+                return -ret;
+            }
+
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0;
+            double div = 1;
+            byte c = read();
+
+            while (c <= ' ') {
+                c = read();
+            }
+
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg) {
+                return -ret;
+            }
+
+            return ret;
+        }
+
+        public void skip(int n) throws IOException {
+            for (int i = 0; i < n; i++) {
+                read();
+            }
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (isEOF()) {
+                buffer[0] = -1;
+            }
+        }
+
+        private byte read() throws IOException {
+            if(isEOF()) {
+                throw new EOFException();
+            }
+            if (bufferPointer == bytesRead) {
+                fillBuffer();
+            }
+
+            return buffer[bufferPointer++];
+        }
+
+        private boolean isEOF() {
+            return bytesRead == -1;
+        }
+
+        public void close() throws IOException {
+            if (din != null) {
+                din.close();
+                din = null;
+            }
+        }
+
+        public boolean isClosed() {
+            return din == null;
+        }
+
+    }
+
 
 }
