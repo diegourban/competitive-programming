7c37100cfa8d4e9d5b73b17327c5d9b3011099f7
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-01-27 15:34:38 -0200

    1848 - Counting Crows

---
+++ uri-online-judge/src/test/java/urban/uri_online_judge/beginner/_1848/MainTest.java
@@ -0,0 +1,14 @@
+package urban.uri_online_judge.beginner._1848;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class MainTest {
+
+    @Test
+    public void shouldCreateInstance() {
+        assertNotNull(new Main());
+    }
+
+}
