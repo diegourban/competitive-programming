b8bd12afbaca8381ae7f708821a9ef839e0f7985
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-01-31 12:14:56 -0200

    2163 - The Force Awakens

---
+++ uri-online-judge/src/test/java/urban/uri_online_judge/beginner/_2163/MainTest.java
@@ -0,0 +1,14 @@
+package urban.uri_online_judge.beginner._2163;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class MainTest {
+
+    @Test
+    public void shouldCreateInstance() {
+        assertNotNull(new Main());
+    }
+
+}
