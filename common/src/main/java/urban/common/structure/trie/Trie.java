1c5fbc1088882a4be8542f65b5eb3436b2d1d066
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-04-15 18:31:49 -0300

    Trie

---
+++ common/src/main/java/urban/common/structure/trie/Trie.java
@@ -0,0 +1,42 @@
+package urban.common.structure.trie;
+
+public class Trie {
+
+    private TrieNode rootNode;
+    private final char endOfWordMarker = '\0';
+
+    public Trie() {
+        this.rootNode = new TrieNode();
+    }
+
+    public boolean checkPresentAndAdd(String word) {
+
+        TrieNode currentNode = this.rootNode;
+        boolean isNewWord = false;
+
+        // Work downwards through the trie, adding nodes
+        // as needed, and keeping track of whether we add
+        // any nodes.
+        for (int i = 0; i < word.length(); i++) {
+            char character = word.charAt(i);
+
+            if (!currentNode.hasChildNode(character)) {
+                isNewWord = true;
+                currentNode.makeChildNode(character);
+            }
+
+            currentNode = currentNode.getChildNode(character);
+        }
+
+        // Explicitly mark the end of a word.
+        // Otherwise, we might say a word is
+        // present if it is a prefix of a different,
+        // longer word that was added earlier.
+        if (!currentNode.hasChildNode(endOfWordMarker)) {
+            isNewWord = true;
+            currentNode.makeChildNode(endOfWordMarker);
+        }
+
+        return isNewWord;
+    }
+}
---
+++ common/src/main/java/urban/common/structure/trie/TrieNode.java
@@ -0,0 +1,24 @@
+package urban.common.structure.trie;
+
+import java.util.HashMap;
+
+class TrieNode {
+
+    private HashMap<Character, TrieNode> nodeChildren;
+
+    public TrieNode() {
+        this.nodeChildren = new HashMap<Character, TrieNode>();
+    }
+
+    public boolean hasChildNode(char character) {
+        return this.nodeChildren.containsKey(character);
+    }
+
+    public void makeChildNode(char character) {
+        this.nodeChildren.put(character, new TrieNode());
+    }
+
+    public TrieNode getChildNode(char character) {
+        return this.nodeChildren.get(character);
+    }
+}
