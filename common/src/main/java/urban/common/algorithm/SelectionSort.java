4718d1d0701e0535104e2c8b555047a43d7eb1cf
Diego L. Urban <diego.urban88@gmail.com>
2017-05-18 12:27:33 -0300

    Add files via upload

--- common/src/main/java/urban/common/algorithm/SelectionSort.java
+++ common/src/main/java/urban/common/algorithm/SelectionSort.java
@@ -4,20 +4,20 @@ import java.util.List;
 
 public class SelectionSort {
 
-    public void sort(final List<Integer> list) {
-        for(int i = 0; i < list.size() - 1; i++) {
-            final int smallerIndex = findSmallerIndex(list, i, list.size());
-            Integer current = list.get(i);
-            Integer smaller = list.get(smallerIndex);
-            list.set(i, smaller);
-            list.set(smallerIndex, current);
+    public void sort(final int[] list, final int totalElements) {
+        for(int i = 0; i < totalElements - 1; i++) {
+            final int smallerIndex = findSmallerIndex(list, i, totalElements);
+            Integer current = list[i];
+            Integer smaller = list[smallerIndex];
+            list[i] = smaller;
+            list[smallerIndex] = current;
         }
     }
 
-    private int findSmallerIndex(final List<Integer> list, final int begin, final int end) {
+    private int findSmallerIndex(final int[] list, final int begin, final int end) {
         int smallerIndex = begin;
         for(int i = begin; i < end; i++) {
-            if(list.get(i) < list.get(smallerIndex)) {
+            if(list[i] < list[smallerIndex]) {
                 smallerIndex = i;
             }
         }
