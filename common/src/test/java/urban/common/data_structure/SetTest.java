16f6078ab93b04d75922f183f51a27c3a889d009
Diego L. Urban <diego.urban88@gmail.com>
2017-05-18 12:23:50 -0300

    Add files via upload

--- common/src/test/java/urban/common/data_structure/SetTest.java
+++ common/src/test/java/urban/common/data_structure/SetTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import urban.common.data_structure.set.Set;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class SetTest {
 
