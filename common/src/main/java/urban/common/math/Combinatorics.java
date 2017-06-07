08853c280353590393b21429610cbc3ba61b0b1a
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-06-07 20:28:50 -0300

    knapsack problem

--- common/src/main/java/urban/common/math/Combinatorics.java
+++ common/src/main/java/urban/common/math/Combinatorics.java
@@ -45,4 +45,69 @@ public class Combinatorics {
         return binomials[m];
     }
 
+    /**
+     * This is a classic computer science puzzle called "the unbounded knapsack problem."
+     * https://en.wikipedia.org/wiki/Knapsack_problem
+     * <p>
+     * We use a bottom-up approach to find the max value at our bag's weightCapacity by finding the max value at every capacity from 0 to weightCapacity.
+     * <p>
+     * Complexity: O(n * k) time and O(k) space, where n is the number of boxes and k is the capacity of the bag.
+     *
+     * @param boxes the boxes available
+     * @param weightCapacity the maximum weight capacity of the bag
+     * @return the maximum value
+     */
+    public static long knapsackProblem(Box[] boxes, int weightCapacity) {
+        // we make an array to hold the maximum possible value at every
+        // bag weight capacity from 0 to weightCapacity
+        // starting each index with value 0
+        long[] maxValuesAtCapacities = new long[weightCapacity + 1];
+
+        for (int currentCapacity = 0; currentCapacity <= weightCapacity; currentCapacity++) {
+
+            // set a variable to hold the max monetary value so far for currentCapacity
+            long currentMaxValue = 0;
+
+            for (Box box : boxes) {
+
+                // if a box weighs 0 and has a positive value the value of our bag is infinite!
+                if (box.weight == 0 && box.value != 0) {
+                    throw new InfinityException();
+                }
+
+                // if the current box weighs as much or less than the current weight capacity
+                // it's possible taking the box would give get a better value
+                if (box.weight <= currentCapacity) {
+
+                    // so we check: should we use the box or not?
+                    // if we use the box, the most kilograms we can include in addition to the box
+                    // we're adding is the current capacity minus the box's weight. we find the max
+                    // value at that integer capacity in our array maxValuesAtCapacities
+                    long maxValueUsingBox = box.value + maxValuesAtCapacities[currentCapacity - box.weight];
+
+                    // now we see if it's worth taking the box. how does the
+                    // value with the box compare to the currentMaxValue?
+                    currentMaxValue = Math.max(maxValueUsingBox, currentMaxValue);
+                }
+            }
+
+            // add each capacity's max value to our array so we can use them
+            // when calculating all the remaining capacities
+            maxValuesAtCapacities[currentCapacity] = currentMaxValue;
+        }
+
+        return maxValuesAtCapacities[weightCapacity];
+    }
+
+    class Box {
+        int weight;
+        int value;
+    }
+
+    static class InfinityException extends RuntimeException {
+        public InfinityException() {
+            super("Max value is infinity!");
+        }
+    }
+
 }
