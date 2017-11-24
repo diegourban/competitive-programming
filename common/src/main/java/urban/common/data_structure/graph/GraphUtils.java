947c4288b260a4d94eaf18cc206855b86bb8ab7b
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-11-24 19:05:49 -0200

    shortest path bfs

--- common/src/main/java/urban/common/data_structure/graph/GraphUtils.java
+++ common/src/main/java/urban/common/data_structure/graph/GraphUtils.java
@@ -1,8 +1,6 @@
 package urban.common.data_structure.graph;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class GraphUtils {
 
@@ -48,5 +46,78 @@ public class GraphUtils {
                 }
             }
         }
+    }
+
+    /**
+     * Solution:
+     * We treat the input user network as a graph in adjacency list format. Then we do a breadth-first search from the sender, stopping once we reach the recipient.
+     * In order to recover the actual shortest path from the sender to the recipient, we do two things:
+     * 1. during our breadth-first search, we keep track of how we reached each node;
+     * 2. after our breadth-first search reaches the end node, we use our hash map to backtrack from the recipient to the sender.
+     * <p>
+     * To make sure our breadth-first search terminates, we're careful to avoid visiting any node twice. We could keep track of the nodes we've already seen in a hash set, but, to save space, we reuse the hash map we've already set up for recovering the path.
+     *
+     * Complexity:
+     *
+     *
+     * @param graph     the network graph
+     * @param startNode the sender
+     * @param endNode   the receiver
+     * @return the shortest path between the sender and receiver
+     */
+    public static String[] shortestPathBfs(Map<String, String[]> graph, String startNode, String endNode) {
+        if (!graph.containsKey(startNode)) {
+            throw new IllegalArgumentException("Start node not in graph");
+        }
+        if (!graph.containsKey(endNode)) {
+            throw new IllegalArgumentException("End node not in graph");
+        }
+
+        Queue<String> nodesToVisit = new ArrayDeque<>();
+        nodesToVisit.add(startNode);
+
+        // keep track of how we got to each node
+        // we'll use this to reconstruct the shortest path at the end
+        // we'll ALSO use this to keep track of which nodes we've
+        // already visited
+        Map<String, String> howWeReachedNodes = new HashMap<>();
+        howWeReachedNodes.put(startNode, null);
+
+        while (!nodesToVisit.isEmpty()) {
+            String currentNode = nodesToVisit.remove();
+
+            // stop when we reach the end node
+            if (currentNode.equals(endNode)) {
+                return reconstructPath(howWeReachedNodes, startNode, endNode);
+            }
+
+            for (String neighbor : graph.get(currentNode)) {
+                if (!howWeReachedNodes.containsKey(neighbor)) {
+                    nodesToVisit.add(neighbor);
+                    howWeReachedNodes.put(neighbor, currentNode);
+                }
+            }
+        }
+
+        // if we get here, then we never found the end node
+        // so there's NO path from startNode to endNode
+        return null;
+    }
+
+    private static String[] reconstructPath(Map<String, String> previousNodes, String startNode, String endNode) {
+        List<String> reversedShortestPath = new ArrayList<>();
+
+        // start from the end of the path and work backwards
+        String currentNode = endNode;
+
+        while (currentNode != null) {
+            reversedShortestPath.add(currentNode);
+            currentNode = previousNodes.get(currentNode);
+        }
+
+        // reverse our path to get the right order
+        // by flipping it around, in place
+        Collections.reverse(reversedShortestPath);
+        return reversedShortestPath.toArray(new String[reversedShortestPath.size()]);
     }
 }
--- common/src/test/java/urban/common/data_structure/graph/GraphUtilsTest.java
+++ common/src/test/java/urban/common/data_structure/graph/GraphUtilsTest.java
@@ -5,7 +5,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class GraphUtilsTest {
@@ -33,5 +35,58 @@ public class GraphUtilsTest {
         List<String> remainingPossibleColors = colors.stream().filter(color -> !color.equals(b.getColor())).collect(Collectors.toList());
         Assert.assertThat(a.getColor(), Matchers.isIn(remainingPossibleColors));
         Assert.assertThat(c.getColor(), Matchers.isIn(remainingPossibleColors));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldHandleNotExistingStartNode() {
+        Map<String, String[]> graph = new HashMap<>();
+        graph.put("Foo", new String[]{"A", "B"});
+
+        GraphUtils.shortestPathBfs(graph, "Bar", "Baz");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldHandleNotExistingEndNode() {
+        Map<String, String[]> graph = new HashMap<>();
+        graph.put("Foo", new String[]{"A", "B"});
+
+        GraphUtils.shortestPathBfs(graph, "Foo", "Baz");
+    }
+
+    @Test
+    public void shouldHandleNoPath() {
+        Map<String, String[]> graph = new HashMap<>();
+        graph.put("Foo", new String[]{"Bar"});
+        graph.put("Bar", new String[]{"Foo"});
+        graph.put("Baz", new String[]{"Asdf"});
+        graph.put("Asdf", new String[]{"Baz"});
+
+        Assert.assertThat(GraphUtils.shortestPathBfs(graph, "Foo", "Asdf"), Matchers.nullValue());
+    }
+
+    @Test
+    public void shouldHandleLoops() {
+        Map<String, String[]> graph = new HashMap<>();
+        graph.put("Foo", new String[]{"Asdf", "Bar"});
+        graph.put("Asdf", new String[]{"Foo", "Bar"});
+        graph.put("Bar", new String[]{"Foo", "Asdf", "Baz"});
+        graph.put("Baz", new String[]{"Bar"});
+
+        Assert.assertThat(GraphUtils.shortestPathBfs(graph, "Foo", "Baz"), Matchers.equalTo(new String[]{"Foo", "Bar", "Baz"}));
+    }
+
+    @Test
+    public void shouldFindRoute() {
+        Map<String, String[]> graph = new HashMap<String, String[]>() {{
+            put("Min", new String[]{"William", "Jayden"});
+            put("William", new String[]{"Min"});
+            put("Jayden", new String[]{"Min", "Amelia", "Ren"});
+            put("Ren", new String[]{"Jayden"});
+            put("Amelia", new String[]{"Jayden", "Adam", "Miguel"});
+            put("Adam", new String[]{"Amelia", "Miguel"});
+            put("Miguel", new String[]{"Amelia", "Adam"});
+        }};
+
+        Assert.assertThat(GraphUtils.shortestPathBfs(graph, "Min", "Adam"), Matchers.equalTo(new String[]{"Min", "Jayden", "Amelia", "Adam"}));
     }
 }
